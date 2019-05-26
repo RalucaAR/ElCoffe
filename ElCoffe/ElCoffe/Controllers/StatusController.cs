8e8278aceebd54a1dc527b1f03dd6243f32a03d5
RalucaAR <34485883+RalucaAR@users.noreply.github.com>
2019-05-26 10:49:20 +0300

    Update StatusController.cs

--- ElCoffe/ElCoffe/Controllers/StatusController.cs
+++ ElCoffe/ElCoffe/Controllers/StatusController.cs
@@ -1,93 +1,72 @@
-﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+using ElCoffe.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
 
 namespace ElCoffe.Controllers
 {
     public class StatusController : Controller
     {
-        // GET: Status
-        public ActionResult Index()
-        {
-            return View();
-        }
+        private DbConn db = new DbConn();
 
-        // GET: Status/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Status/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Status/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Status/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Status/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Status/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Status/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+    // GET: api/Todo
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Status>>> GetAll()
+    {
+        return await db.Statuses.ToListAsync();
     }
+
+    // GET: api/Todo/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Status>> GetStatus(long id)
+    {
+        var sts = await db.Statuses.FindAsync(id);
+
+        if (sts == null)
+        {
+            return NotFound();
+        }
+
+        return sts;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Status>> Create([FromBody]Status sts)
+    {
+        db.Statuses.Add(sts);
+        await db.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetStatus), new { id = sts.Id }, sts);
+    }
+
+        // PUT: api/Todo/5
+        [HttpPut]
+        public async Task<IActionResult> UpdateReservation(Status sts)
+        {
+            db.Entry(sts).State = EntityState.Modified;
+            await db.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/Todo/5
+        [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteStatus(long id)
+    {
+        var sts = await db.Statuses.FindAsync(id);
+
+        if (sts == null)
+        {
+            return NotFound();
+        }
+
+        db.Statuses.Remove(sts);
+        await db.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
 }
